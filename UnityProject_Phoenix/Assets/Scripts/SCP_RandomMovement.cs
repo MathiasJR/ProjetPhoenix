47a5e99871cf63f2d2832b752bbaf2ba356591bf
MathiasJR <mathias.facebook@gmail.com>
2017-12-11 03:25:43 +0100

    Tempest and tornado tests

---
+++ UnityProject_Phoenix/Assets/Scripts/SCP_RandomMovement.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SCP_RandomMovement : MonoBehaviour
+{
+
+    public float velocidadMax;
+
+    public float xMax;
+    public float zMax;
+    public float xMin;
+    public float zMin;
+
+    private float x;
+    private float z;
+    private float tiempo;
+    private float angulo;
+
+    // Use this for initialization
+    void Start()
+    {
+
+
+        x = Random.Range(-velocidadMax, velocidadMax);
+        z = Random.Range(-velocidadMax, velocidadMax);
+        angulo = Mathf.Atan2(x, z) * (180 / 3.141592f) + 90;
+        transform.localRotation = Quaternion.Euler(0, angulo, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        tiempo += Time.deltaTime;
+
+        if (transform.localPosition.x > xMax)
+        {
+            x = Random.Range(-velocidadMax, 0.0f);
+            angulo = Mathf.Atan2(x, z) * (180 / 3.141592f) + 90;
+            transform.localRotation = Quaternion.Euler(0, angulo, 0);
+            tiempo = 0.0f;
+        }
+        if (transform.localPosition.x < xMin)
+        {
+            x = Random.Range(0.0f, velocidadMax);
+            angulo = Mathf.Atan2(x, z) * (180 / 3.141592f) + 90;
+            transform.localRotation = Quaternion.Euler(0, angulo, 0);
+            tiempo = 0.0f;
+        }
+        if (transform.localPosition.z > zMax)
+        {
+            z = Random.Range(-velocidadMax, 0.0f);
+            angulo = Mathf.Atan2(x, z) * (180 / 3.141592f) + 90;
+            transform.localRotation = Quaternion.Euler(0, angulo, 0);
+            tiempo = 0.0f;
+        }
+        if (transform.localPosition.z < zMin)
+        {
+            z = Random.Range(0.0f, velocidadMax);
+            angulo = Mathf.Atan2(x, z) * (180 / 3.141592f) + 90;
+            transform.localRotation = Quaternion.Euler(0, angulo, 0);
+            tiempo = 0.0f;
+        }
+
+
+        if (tiempo > 1.0f)
+        {
+            x = Random.Range(-velocidadMax, velocidadMax);
+            z = Random.Range(-velocidadMax, velocidadMax);
+            angulo = Mathf.Atan2(x, z) * (180 / 3.141592f) + 90;
+            transform.localRotation = Quaternion.Euler(0, angulo, 0);
+            tiempo = 0.0f;
+        }
+
+        transform.localPosition = new Vector3(transform.localPosition.x + x, transform.localPosition.y, transform.localPosition.z + z);
+    }
+
+
+}
---
+++ UnityProject_Phoenix/Assets/Scripts/SCP_RotateAround.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SCP_RotateAround : MonoBehaviour
+{
+
+    public Transform centerObject;
+
+    public float movementSpeed = 1f;
+    public bool clockwise = true;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void FixedUpdate()
+    {
+        this.transform.LookAt(centerObject);
+        if (clockwise == true)
+        {
+            this.transform.Translate(Vector3.left * movementSpeed);
+        }
+        else
+        {
+            this.transform.Translate(Vector3.right * movementSpeed);
+        }
+        
+    }
+
+}
